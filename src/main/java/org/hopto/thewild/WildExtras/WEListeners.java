fe087b9b53752d8700e98b10918f388c1d134119
David Precious <davidp@preshweb.co.uk>
2016-04-11 22:33:55 +0100

    Revert "--Prevent Elytra"
    This reverts commit 213c31b8b0f3ffc9033dc0e2656e6f063f545a33.
    
    Let's see how we get on with Elytra now 1.9 has stablised a lot.
    
    Also, the current behaviour, since a recent 1.9 API update I think, means that
    your Elytra *disappears* when you try to move it into a chest, which is pretty
    unfair!

--- src/main/java/org/hopto/thewild/WildExtras/WEListeners.java
+++ src/main/java/org/hopto/thewild/WildExtras/WEListeners.java
@@ -84,10 +84,8 @@ public class WEListeners implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
     	HumanEntity human =  event.getView().getPlayer();
-	    if(human instanceof Player) {
-	    	if (event.getCurrentItem().getType() == Material.ELYTRA) {
-	    		human.getInventory().remove(Material.ELYTRA);
-	    	}
+	    if(human instanceof Player)
+	    {
 	        Player player = (Player)human;
 	        if (player.getWorld().getName().contains("old")) {
 		      Inventory inv = event.getInventory();
@@ -420,7 +418,6 @@ public class WEListeners implements Listener {
     public void joinEvent(PlayerJoinEvent event) {
         final Player player = event.getPlayer();
         String playername = player.getName();
-    	player.getInventory().remove(Material.ELYTRA);
         File playerfile = new File("plugins/WildExtras/"+playername);
         if (!playerfile.exists()) {
             debugmsg("New player never seen before!");
